8bd58d621faada9615cd3448eff50e566aa0b380
Breelef <emil_vinther@hotmail.com>
2024-05-28 12:29:50 +0200

    fixes

--- utils/cleanData.py
+++ utils/cleanData.py
@@ -3,8 +3,6 @@ import re
 
 # Define the cleaning function
 def clean_text(text):
-    if pd.isnull(text):
-        return ''
     text = text.lower()  # Lowercase text
     text = re.sub(r'\b\w{1,2}\b', '', text)  # Remove short words that are 1-2 letters long
     text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
@@ -20,13 +18,7 @@ data.drop(columns='Unnamed: 0', inplace=True)
 # Clean the text data
 data['cleaned_text'] = data['text'].apply(clean_text)
 
-# Handle any remaining NaNs in 'cleaned_text'
-data['cleaned_text'] = data['cleaned_text'].fillna('')
-
-# Retain only the 'cleaned_text' and 'label' columns, renaming 'cleaned_text' back to 'text'
-cleaned_data = data[['cleaned_text', 'label']].rename(columns={'cleaned_text': 'text'})
-
 # Write the cleaned data to a new CSV file
-cleaned_data.to_csv('../data/emotions_cleaned.csv', index=False)
+data.to_csv('../data/emotions_cleaned.csv', index=False)
 
 print("Data cleaned and saved to '../data/emotions_cleaned.csv'")
